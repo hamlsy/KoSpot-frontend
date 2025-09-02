3d38cd060383b68423a9c5d409c303a93445c5d8
hamlsy <dltmddud1122@naver.com>
2025-09-02 20:25:47 +0900

    feat: 게임 위치 데이터 로딩 시점을 "시작하기" 클릭 시로 변경하고, ShopView에서 shopService 접근 방식을 개선하며, 라우터에 shopRoutes 추가 #23

---
+++ src/router/shopRoutes.js
@@ -0,0 +1,11 @@
+import ShopView from "@/features/shop/views/ShopView.vue";
+
+const shopRoutes = [
+    {
+        path: '/shop',
+        name: "Shop",
+        component: ShopView
+    }
+]
+
+export default shopRoutes;
